153a3fceb1f3565eabc2b89248618085f9e1379e
Arwin Clyde B. Simondac <ghostwrexker@gmail.com>
2026-09-21 16:51:07 +0800

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Blinker } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 const blinker = Blinker({
   subsets: ["latin"],
@@ -47,6 +48,7 @@ export default function RootLayout({
         <Navbar />
         <main>{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
